5b2947a160f4845d1785001d78c711f10e7e1603
R4Y815 <Akcent360@gmail.com>
2022-03-31 12:07:10 +0800

    first commit

---
+++ planning.js
@@ -0,0 +1,34 @@
+/**
+ * Section1:  HENSHIN MECHANIC
+ * User is presented with 4 cards and/or gets assigned to 1 out of the 4 aces random
+ * PseudoCode: 
+ * 1) Show 4 aces face down
+ * 2) based on what user clicks, turn over the card
+ * 3) show photo of rider activated,
+ * 4) play sound and gif 'henshin'
+ * 
+ * Section 2: Link Attacks, Story, photos, Music, Gifs available to the the user based on the rider hero 
+ * 
+ * Section 3: Each Round/Game instance
+ * PseudoCode:
+ * 1) Draw 4? cards 
+ * 2) 1st: Attack 2nd: Defense 3rd: Store Power? 
+ * 
+ * 
+ * Section 3.1: 
+ * PseudoCode: 
+ * 1) 
+ * 2) 
+ * 
+ * 
+ * Finisher: 
+ *  Combinations of cards [ make excel table of possible card combinations]
+ * 
+ * 
+ * 
+ * Section 4: 
+ * End of Game: 
+ * 1) Show Finishing Move 
+ * 2) Win or Lose
+ * 
+ */
