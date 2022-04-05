038743ee9aab092075a2b4964b5e55d8e596b0a5
R4Y815 <Akcent360@gmail.com>
2022-04-05 09:34:36 +0800

    cleaned up folders and removed unnecessary files

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   development: {
-    username: 'raytor27',
+    username: '<INSERT PSQL USERNAME HERE>',
     password: null,
-    database: 'krbrade_db',
+    database: '615_template_db',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
