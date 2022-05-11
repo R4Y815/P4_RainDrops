d5d71cdf53fb109af0bb9f9a1f75bdf3f2122b57
R4Y815 <Akcent360@gmail.com>
2022-05-11 08:44:19 +0800

    Autoloading of audioPlayers-recommited

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -7,7 +7,6 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Galleria from './Galleria.jsx';
-import AudioPlayer2B from './AudioPlayer2B.jsx';
 
 export default function Slideshow() {
   // STATE Variables Read here
